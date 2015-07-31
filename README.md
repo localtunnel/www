61db1e324fd77beb0d79eee9a0de15165297ca94
Florian <florian@adagioagency.com>
2015-07-31 22:12:32 +0200

    added blank headstart proj

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Headstart Boilerplate 1.2.1](http://headstart.io)
+
+This repo contains the default files used to scaffold your [Headstart project](http://headstart.io).
+Licensed under MIT.
---
+++ assets/js/_view-index.js
@@ -0,0 +1,40 @@
+(function(window, $){
+
+'use strict';
+
+// Underscore is bundled in with ender, so just require it
+// Comment out this line when using the separated Underscore file,
+// eg. when using jQuery
+var _ = require('underscore');
+
+// Document -------------------------------------------------------------------
+
+// When using jQuery, use
+// $(document).ready(function () {
+
+$.domReady(function () {
+
+	console.log('## Index script ready');
+
+	// VARS -------------------------------------------------------------------
+	//
+
+	var
+		$window = $(window),
+		$body = $('body')
+	;
+
+	// ACTIONS ----------------------------------------------------------------
+	//
+
+	// $elem.on('something', doSomething);
+
+	//
+	// FUNCTIONS --------------------------------------------------------------
+	//
+
+	// function doSomething () {}
+
+});
+
+}(window, $));
